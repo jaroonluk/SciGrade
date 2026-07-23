f26334d7d32f0e6fe97fcf7481e100b216e56304
Jaroonluk Trongdee <jaroonluk@kku.ac.th>
2026-07-23 15:29:32 +0700

    update project files

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,6 +22,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
         Gate::policy(GradeReport::class, GradeReportPolicy::class);
     }
 }
